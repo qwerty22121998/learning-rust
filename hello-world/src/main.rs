fc00379b7378359ddf41d87a8c7aca8a27026182
vuhk <qwerty22121998@gmail.com>
2021-01-14 22:57:43 +0700

    println and format

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello, world!");
+    println!("{0}, {1}!", "Hello", "world");
+    println!("{who} {what}", who="Your mom", what="so fat");
+
+    let s = format!("{a} {b}",a="Your mom so fat,", b ="I took a picture of her last Christmas and it's still printing");
+
+    println!("{}", s);
+
 }

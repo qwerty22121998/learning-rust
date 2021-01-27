1efb81fa256bf3c7e700f2d2b9a206e9030e5cdd
vuhk <vuhk@ghtk.vn>
2021-01-27 11:43:39 +0700

    clock

---
+++ exercism/rust/clock/src/lib.rs
@@ -0,0 +1,26 @@
+use std::fmt;
+
+#[derive(PartialEq, Eq, Debug)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
+
+impl Clock {
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        let add = minutes / 60 - (minutes % 60 < 0) as i32;
+        let minutes = (minutes % 60 + 60) % 60;
+        let hours = ((hours + add) % 24 + 24) % 24;
+        Clock { hours, minutes }
+    }
+
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        Clock::new(self.hours, self.minutes + minutes)
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:0>2}:{:0>2}", self.hours, self.minutes)
+    }
+}
